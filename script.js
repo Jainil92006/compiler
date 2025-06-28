b2c8029c949db6c0bd1e761ae915e4590fb399d5
Jainil Patel <131519763+Jainil92006@users.noreply.github.com>
2025-06-28 10:55:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function run() {
+        let htmlcode = document.getElementById("html-code").value;
+        let CssCode = document.getElementById("Css-code").value;
+        let jsCode = document.getElementById("JS-code").value;
+        let output = document.getElementById("output");
+
+        output.contentDocument.body.innerHTML =
+          htmlcode + "<style>" + CssCode + "</style>";
+
+        output.contentWindow.eval(jsCode);
+      }
